180f112576289748ca92622dc9494d06fe78e782
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-06-28 11:44:04 -0400

    first commit

--- Main.java
+++ Main.java
@@ -2,6 +2,51 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String name = "Abdoul";
+
+
+
+
+        System.out.println(name.charAt(0));
+
+        if(name.charAt(0) == 'A') {
+            System.out.println("Name starts with A");
+        }
+        else {
+            System.out.println("Name does not start with A");
+            System.out.println(Escape());
+        }
+
+    }
+
+    static  int mathOne() {
+        return 0;
+
+        // ** Wrapper Classes**: In java, wrapper classes turn primitive data types (int, char, double, etc.) into objects.
+
+        // **Autoboxing**: the automatic conversion that the java compiler makes between the primitive types and their respective primitive types.
+
+        // **Unboxing**: The reverse of autoboxing. It is the automatic conversion of wrapper classes back to their respective primitive types.
+
+        // AutoBoxing//
+
+        Integer Myinteger = 25;
+
+        int num = Myinteger;
+
+        return num;
+    }
+
+        static String escape(){
+            return "\"Gustavo\" \nBaez";
+
+            System.out.println();
+        }
+
+
+        static double mathclass
 
 
     }
